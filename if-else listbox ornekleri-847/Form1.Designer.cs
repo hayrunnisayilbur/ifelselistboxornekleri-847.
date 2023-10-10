70e0deacdc2ba9136a7843b0e578e7f954eb83ff
hayrunnisayilbur <145451185+hayrunnisayilbur@users.noreply.github.com>
2023-10-10 16:39:43 +0300

    Add files via upload

---
+++ if-else listbox ornekleri-847/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace if_else_listbox_ornekleri_847
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTek = new System.Windows.Forms.Label();
+            this.lblCift = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.txtSayi = new System.Windows.Forms.TextBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lblTek
+            // 
+            this.lblTek.AutoSize = true;
+            this.lblTek.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTek.Location = new System.Drawing.Point(20, 133);
+            this.lblTek.Name = "lblTek";
+            this.lblTek.Size = new System.Drawing.Size(91, 24);
+            this.lblTek.TabIndex = 0;
+            this.lblTek.Text = "tek sayılar";
+            // 
+            // lblCift
+            // 
+            this.lblCift.AutoSize = true;
+            this.lblCift.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblCift.Location = new System.Drawing.Point(229, 133);
+            this.lblCift.Name = "lblCift";
+            this.lblCift.Size = new System.Drawing.Size(89, 24);
+            this.lblCift.TabIndex = 1;
+            this.lblCift.Text = "çift sayılar";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(24, 172);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(160, 264);
+            this.listBox1.TabIndex = 2;
+            // 
+            // txtSayi
+            // 
+            this.txtSayi.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSayi.Location = new System.Drawing.Point(24, 53);
+            this.txtSayi.Name = "txtSayi";
+            this.txtSayi.Size = new System.Drawing.Size(160, 29);
+            this.txtSayi.TabIndex = 3;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEkle.Location = new System.Drawing.Point(233, 53);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(162, 29);
+            this.btnEkle.TabIndex = 4;
+            this.btnEkle.Text = "ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(233, 172);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(162, 264);
+            this.listBox2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.txtSayi);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.lblCift);
+            this.Controls.Add(this.lblTek);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTek;
+        private System.Windows.Forms.Label lblCift;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox txtSayi;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ if-else listbox ornekleri-847/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace if_else_listbox_ornekleri_847
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            int sayi = Convert.ToInt32(txtSayi.Text);
+            //çift kontrolü sayi %2 == 0 sayı çiftdir.
+            if(sayi %2 == 0)
+            {
+                listBox1.Items.Add(txtSayi.Text);
+            }
+            else
+            {
+                listBox2.Items.Add(txtSayi.Text);
+            }
+        }
+    }
+}
